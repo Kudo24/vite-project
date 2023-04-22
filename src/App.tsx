ca9d1bc26ce60d88b8ef0647a3f301e83dc5eca2
Kudo24 <alistaire024@gmail.com>
2023-04-22 11:52:29 +0800

    1st phase

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import List from "./components/List";
@@ -6,14 +6,32 @@ import Button from "./components/Button";
 
 function App() {
   const [appear, setAppear] = useState(false);
+
   const handleList = (item: string) => {
     console.log(item);
   };
 
+  const [name, setname] = useState<string>("");
+  const [addnames, setAddNames] = useState<string[]>([]);
+
+  const getName = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setAddNames([...addnames, name]);
+    setname("");
+  };
+  console.log(name);
   return (
     <>
       {appear ? (
-        <List handleList={handleList} onclick={() => setAppear(false)} />
+        <List
+          handleList={handleList}
+          onclick={() => setAppear(false)}
+          name={name}
+          setname={setname}
+          addnames={addnames}
+          getName={getName}
+        />
       ) : null}
 
       <Button name="hello" onclick={() => setAppear(true)} />
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,21 +4,28 @@ import { useEffect, useState } from "react";
 interface Props {
   handleList: (item: string) => void;
   onclick: () => void;
+  name: string;
+  setname: React.Dispatch<React.SetStateAction<string>>;
+  addnames: string[];
+  getName: (e: React.FormEvent) => void;
 }
 
-function List({ handleList, onclick }: Props) {
+function List({ handleList, onclick, addnames, setname, getName }: Props) {
   const list = ["alistaire", "andrei", "peter"];
   const [color, setcolor] = useState("");
 
   return (
     <div>
       <div>
-        <form action="">
-          <input type="text" name="" id="" />
+        <form action="" onSubmit={getName}>
+          <input
+            type="text"
+            name=""
+            id=""
+            onChange={(e) => setname(e.target.value)}
+          />
 
-          <button type="submit" onClick={() => console.log("submit")}>
-            submit
-          </button>
+          <button type="submit">submit</button>
         </form>
         <ul className="list-group">
           {list.map((list, index) => (
@@ -33,6 +40,11 @@ function List({ handleList, onclick }: Props) {
         </ul>
         <button onClick={onclick}>close</button>
       </div>
+      <p>
+        {addnames.map((adname) => (
+          <li>{adname}</li>
+        ))}
+      </p>
     </div>
   );
 }
